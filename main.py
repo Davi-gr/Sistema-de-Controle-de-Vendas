d4611812df46fc29e3868d7705a8ef530e6306fb
Davi Rocha <davigomroc@gmail.com>
2026-06-22 13:03:40 -0300

    Adicionada função para Registro de Vendas

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ class Produto:
                 raise ValueError("O Preço de venda não pode ser menor que o Preço de compra")
             self.preco_venda = preco_venda
 
-            if estoque < 0:
-                raise ValueError("Estoque não pode ser negativo")
             self.estoque = estoque
             
 
@@ -61,5 +59,50 @@ def listar_produtos():
         print("Preço de venda:", produto.preco_venda)
         print("Estoque:", produto.estoque)
 
+#Função que registra vendas retorna um dicionário
 def registrar_venda():
-    
+    id_produto_vendido = int(input("Digite o ID do produto:"))
+
+    for produto in produtos:
+        if produto.numid == id_produto_vendido:
+            
+            #Verificando Estoque -----------------------------
+            quantidade_vendida = int(input("Quantidade vendida: "))
+            if quantidade_vendida <= 0:
+                print("Quantidade inválida.")
+                return None
+            if quantidade_vendida > produto.estoque:
+                print("Quantidade de estoque ficará negativa!")
+                continuar = input("Deseja continuar a venda? [s/n]")
+                if continuar != "s":
+                    return None
+           
+
+            #Desconto-----------------------------------------
+            desconto = float(input("Desconto: "))
+            if desconto < 0:
+                print("Desconto inválido.")
+                return None
+            if desconto > produto.preco_venda - produto.preco_compra:
+                print ("Você está tendo prejuízo!")
+                continuar = input("Deseja continuar a venda? [s/n]")
+                if continuar != "s":
+                    return None
+            
+            produto.estoque -= quantidade_vendida
+            preco_final_unitario = produto.preco_venda - desconto
+            total = preco_final_unitario * quantidade_vendida
+            lucro = (total - produto.preco_compra) *quantidade_vendida
+
+            return {
+            "produto": produto.nome,
+            "quantidade": quantidade_vendida,
+            "total": total,
+            "lucro": lucro
+            }
+    return None        
+
+venda = registrar_venda()
+
+if venda is not None:
+    print("Total da venda:", venda["total"])
